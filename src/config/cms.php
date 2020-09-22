ebb642141109a4b1ac479c612bb0d1e70535226a
mung9thang12 <mung9thang12@gmail.com>
2020-09-22 23:18:31 +0700

    update config provider

--- src/config/cms.php
+++ src/config/cms.php
@@ -2,7 +2,6 @@
 return [
 
     'providers' => [
-        \Mung9thang12\Admin\Providers\AdminServiceProvider::class,
         \Mung9thang12\Permission\PermissionServiceProvider::class,
     ],
     'aliases' => [],
